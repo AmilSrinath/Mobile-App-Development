6e7cda453643dc0c3040c912be363a4e2b32ce12
AmilSrinath <amilsrinath5@gmail.com>
2024-07-04 12:48:01 +0530

    add image and button for home screen

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,15 +1,58 @@
-import { Text, View } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Button,
+  Alert,
+  Image,
+  TextInput,
+} from "react-native";
+import React from "react";
 
-export default function Index() {
+export default function index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text style={style.nametext}>Amil Srinath</Text>
+
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          margin: 20,
+          borderRadius: 20,
+          textAlign: "center",
+        }}
+        placeholder="Type here.."
+        onChangeText={(text) => console.log(text)}
+      />
+
+      <Button
+        onPress={() => console.log("hello")}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+      <Image
+        source={require("../assets/images/react-logo.png")}
+        style={{
+          width: 200,
+          height: 200,
+          alignSelf: "center",
+        }}
+      />
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  nametext: {
+    fontSize: 30,
+    color: "red",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 100,
+  },
+});
